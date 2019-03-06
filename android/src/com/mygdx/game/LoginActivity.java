d98061739089af111122f0d98cb0dc3ce0ad17b7
ramialnaser <raminasruk@gmail.com>
2019-03-06 14:01:34 +0100

    FaceBook implementation and button

--- android/src/com/mygdx/game/LoginActivity.java
+++ android/src/com/mygdx/game/LoginActivity.java
@@ -7,11 +7,23 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends Activity {
 
     private Button login;
-
+    private LoginButton faceLog;
+    private TextView staus;
+    private CallbackManager callbackManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -19,7 +31,9 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login_layout);
 
         login = findViewById(R.id.loginButton);
-
+        faceLog = findViewById(R.id.facelog);
+        staus = findViewById(R.id.status);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         SharedPreferences pref = getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE);
         final SharedPreferences.Editor editor = pref.edit();
 
@@ -36,5 +50,31 @@ public class LoginActivity extends Activity {
                 finish();
             }
         });
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+            //System.out.println("passed");
+                staus.setText("Good\n"+loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+               // System.out.print("awdd");
+                staus.setText("Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
