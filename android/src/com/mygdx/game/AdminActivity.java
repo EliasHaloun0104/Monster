1dc8cd010b0b3c791ca08ddb064acb4dab3342c0
msalaheldin <Mohamed.abdelhalem0016@stud.hkr.se>
2019-03-24 15:01:06 +0100

    login to the corresponding activity

--- android/src/com/mygdx/game/AdminActivity.java
+++ android/src/com/mygdx/game/AdminActivity.java
@@ -1,14 +1,49 @@
 package com.mygdx.game;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    private Button signOutButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-}
+
+        mAuth = FirebaseAuth.getInstance();
+        signOutButton = findViewById(R.id.admin_signOutBtn);
+
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(AdminActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
 }
--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -3,14 +3,18 @@ package com.mygdx.game;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AndroidLauncher extends Activity {
 
@@ -23,7 +27,6 @@ public class AndroidLauncher extends Activity {
     private Button signOutButton;
     private Button shareButton;
     private LoginManager loginManager;
-
 
 
     //Firebase
@@ -54,7 +57,6 @@ public class AndroidLauncher extends Activity {
 
         //Firebase
         mAuth = FirebaseAuth.getInstance();
-
 
 
         playButton = findViewById(R.id.startGame);
@@ -140,7 +142,31 @@ public class AndroidLauncher extends Activity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null){
+            DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+            DatabaseReference userRef = rootRef.child("Users").child(currentUser.getUid());
 
+            try {
+
+                userRef.child("role").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getValue().equals("admin")) {
+                            Intent mainIntent = new Intent(AndroidLauncher.this, AdminActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            } catch (Exception ex) {
+            }
+        }
         // to check if it's logged in or not
         if (currentUser == null) {
             Intent intent = new Intent(AndroidLauncher.this, LoginActivity.class);
