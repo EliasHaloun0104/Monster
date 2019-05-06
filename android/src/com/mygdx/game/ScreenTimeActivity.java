65a439a8b7adec606f9d180b88f2526a37eeaa82
msalaheldin <mohamed.abdelhalem0016@stud.hkr.se>
2019-05-06 13:12:57 +0200

    fetch data and display it on the timer activity

--- android/src/com/mygdx/game/ScreenTimeActivity.java
+++ android/src/com/mygdx/game/ScreenTimeActivity.java
@@ -5,14 +5,17 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ScreenTimeActivity extends AppCompatActivity {
 
     private TextView todayText;
-    private TextView weekText;
     private TextView monthText;
     private TextView yearText;
 
 
+    DatabaseHelper myDb;
 
 
     @Override
@@ -21,8 +24,39 @@ public class ScreenTimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_screen_time);
 
         todayText = findViewById(R.id.screentime_todayTime);
-        weekText = findViewById(R.id.screentime_time7days);
-        monthText = findViewById(R.id.screentime_time30days);
-        yearText = findViewById(R.id.screentime_time12month);
+        monthText = findViewById(R.id.screentime_month);
+        yearText = findViewById(R.id.screentime_year);
+
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        myDb = new DatabaseHelper(this);
+
+        long today = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 1);
+
+
+        todayText.setText(theDiff(today));
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM");
+
+        long thisMonth = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 2);
+        monthText.setText(theDiff(thisMonth));
+
+        simpleDateFormat = new SimpleDateFormat("yyyy");
+        long thisYear = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 3);
+        yearText.setText(theDiff(thisYear));
+    }
+    public String theDiff(long ms) {
+
+        long totalSecs = ms / 1000;
+
+        long diffHours = (totalSecs / 3600);
+        long diffMins = (totalSecs / 60) % 60;
+        long diffSecs = totalSecs % 60;
+
+
+        return diffHours + "h " + diffMins + "m " + diffSecs + "s";
+
+
     }
 }
