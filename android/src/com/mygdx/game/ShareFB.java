a8069cf6ef42c5a8517b2318d330a43bf8d3045f
msalaheldin <mohamed.abdelhalem0016@stud.hkr.se>
2019-05-06 13:41:46 +0200

    timer function ShareFBActivity

--- android/src/com/mygdx/game/ShareFB.java
+++ android/src/com/mygdx/game/ShareFB.java
@@ -27,6 +27,9 @@ import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import retrofit2.Response;
 
 
@@ -35,9 +38,24 @@ public class ShareFB extends AppCompatActivity {
     private ShareDialog shareDialog;
     CallbackManager callbackManager;
 
+    // timer and SQLite database
+    DatabaseHelper myDb;
+    private long startTime;
+    private long endTime;
+    private Date currentDate;
+
+    SimpleDateFormat simpleTimeFormat;
+    SimpleDateFormat simpleDateFormat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        myDb = new DatabaseHelper(this);
+        simpleTimeFormat = new SimpleDateFormat("HH:mm:ss");
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        currentDate = new Date(System.currentTimeMillis());
 
         callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
@@ -118,6 +136,23 @@ public class ShareFB extends AppCompatActivity {
             ShareDialog.show(ShareFB.this, content);
         }
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startTime = System.currentTimeMillis();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        endTime = System.currentTimeMillis();
+        long diff = endTime - startTime;
+        String theDate = simpleDateFormat.format(currentDate);
+        TheTimer theTimer = new TheTimer(theDate, diff);
     }
 
 
