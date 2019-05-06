3c310b2bd136b7eb34a7c6c742b4202d1b5b5305
msalaheldin <mohamed.abdelhalem0016@stud.hkr.se>
2019-05-06 13:28:06 +0200

    timer function StatusActivity

--- android/src/com/mygdx/game/StatusActivity.java
+++ android/src/com/mygdx/game/StatusActivity.java
@@ -21,6 +21,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class StatusActivity extends AppCompatActivity {
 
     private Button saveStatus;
@@ -34,10 +37,25 @@ public class StatusActivity extends AppCompatActivity {
     //progress dialog
     private ProgressDialog progressDialog;
 
+    // timer and SQLite database
+    DatabaseHelper myDb;
+    private long startTime;
+    private long endTime;
+    private Date currentDate;
+
+    SimpleDateFormat simpleTimeFormat;
+    SimpleDateFormat simpleDateFormat;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        myDb = new DatabaseHelper(this);
+        simpleTimeFormat = new SimpleDateFormat("HH:mm:ss");
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        currentDate = new Date(System.currentTimeMillis());
 
         saveStatus = findViewById(R.id.status_newStatusBtn);
         newStatus = findViewById(R.id.status_newStatus);
@@ -82,6 +100,24 @@ public class StatusActivity extends AppCompatActivity {
 
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startTime = System.currentTimeMillis();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        endTime = System.currentTimeMillis();
+        long diff = endTime - startTime;
+        String theDate = simpleDateFormat.format(currentDate);
+        TheTimer theTimer = new TheTimer(theDate, diff);
     }
 
 
