2aa32bd350c30faec126fe82fb4a47ee9f10b11e
EliasHaloun0104 <elias.haloun0104@stud.hkr.se>
2019-06-09 12:28:07 +0200

    Update layout to contain button Prepare body message

--- android/src/com/mygdx/game/ScreenTimeActivity.java
+++ android/src/com/mygdx/game/ScreenTimeActivity.java
@@ -4,6 +4,7 @@ package com.mygdx.game;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ public class ScreenTimeActivity extends AppCompatActivity {
     private TextView yearText;
 
     private Button updateBtn;
+    private Button parentBtn;
 
 
     DatabaseHelper myDb;
@@ -32,6 +34,7 @@ public class ScreenTimeActivity extends AppCompatActivity {
         monthText = findViewById(R.id.screentime_month);
         yearText = findViewById(R.id.screentime_year);
         updateBtn = findViewById(R.id.screentime_updateBtn);
+        parentBtn = findViewById(R.id.screentime_parentBtn);
 
         myDb = new DatabaseHelper(this);
 
@@ -44,9 +47,12 @@ public class ScreenTimeActivity extends AppCompatActivity {
                 displayData();
             }
         });
-
-
-
+        parentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataParentControl();
+            }
+        } );
 
     }
 
@@ -79,5 +85,29 @@ public class ScreenTimeActivity extends AppCompatActivity {
         simpleDateFormat = new SimpleDateFormat("yyyy");
         long thisYear = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 3);
         yearText.setText(theDiff(thisYear));
+    }
+
+
+    public void dataParentControl(){
+        String text = "";
+        Date date = new Date(System.currentTimeMillis());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        long today = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 1);
+        text += "Today: " + theDiff(today) +"\n";
+
+
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM");
+        long thisMonth = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 2);
+        text += "Month: " + theDiff(thisMonth) +"\n";
+
+
+        simpleDateFormat = new SimpleDateFormat("yyyy");
+        long thisYear = myDb.convertDataToString(String.valueOf(simpleDateFormat.format(date)), 3);
+        text += "Year: " + theDiff(thisYear) +"\n";
+        Log.println(Log.ASSERT,"HHH", text);
+
+
     }
 }
